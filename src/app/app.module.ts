731f74b83dec999588dcdeb4c03558d05438ad26
/ohnsonWTC <johnsondubula@gmail.com>
2022-11-14 21:39:33 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DoorComponent } from './door/door.component';
+import { DoorListComponent } from './door-list/door-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoorComponent,
+    DoorListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/door-list/door-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-door-list',
+  templateUrl: './door-list.component.html',
+  styleUrls: ['./door-list.component.css']
+})
+export class DoorListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
