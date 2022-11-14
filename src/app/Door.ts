a666261e7b15e506ea71e7b8cfcd4f327c24289e
/ohnsonWTC <johnsondubula@gmail.com>
2022-11-14 21:43:54 +0200

    init

---
+++ src/app/Door.ts
@@ -0,0 +1,4 @@
+export interface Door{
+    Size : number;
+    Color : string
+}
--- src/app/door/door.component.ts
+++ src/app/door/door.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Door } from '../Door';
 
 @Component({
   selector: 'app-door',
@@ -8,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class DoorComponent implements OnInit {
 
   constructor() { }
+
+  Doorlist : Door[] = [];
 
   ngOnInit(): void {
   }
